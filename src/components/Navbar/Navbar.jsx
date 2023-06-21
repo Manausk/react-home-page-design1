df769f7f969da806674d350ea6ee216adcf16423
ManausK <1905246@kiit.ac.in>
2023-06-21 12:58:00 +0530

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div class="Container">
+      <div className="left">Left</div>
+      <div className="center">center</div>
+      <div className="right">right</div>
+    </div>
+  );
+};
+
+export default Navbar;
